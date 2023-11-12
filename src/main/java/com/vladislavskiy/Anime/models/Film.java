ca33b3d0da9cdd7e48a019fcad99b003ada205b6
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-12 03:04:44 +0200

    added film

---
+++ src/main/java/com/vladislavskiy/Anime/models/Film.java
@@ -0,0 +1,26 @@
+package com.vladislavskiy.Anime.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "films")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double rating;
+    private String description;
+    @JsonIgnore
+    @Transient
+    private String contentURL;         // Local url on local disk
+}
+
