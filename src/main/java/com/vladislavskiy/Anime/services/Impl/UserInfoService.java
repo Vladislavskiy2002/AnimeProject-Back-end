08483bc20e1b7cb7b4f37a58557822605295e702
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-12 03:07:18 +0200

    added UserInfoService

---
+++ src/main/java/com/vladislavskiy/Anime/services/Impl/UserInfoService.java
@@ -0,0 +1,39 @@
+package com.vladislavskiy.Anime.services.Impl;
+
+
+import com.vladislavskiy.Anime.entity.UserInfo;
+import com.vladislavskiy.Anime.repositories.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+    @Autowired
+    private UserInfoRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<UserInfo> userDetail = repository.findByName(username);
+
+        // Converting userDetail to UserDetails
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+}
+
