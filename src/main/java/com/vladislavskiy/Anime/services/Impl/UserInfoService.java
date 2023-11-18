24f00e00044260545eeb81e8deb978968b184619
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-19 01:40:35 +0200

    cleaned code

--- src/main/java/com/vladislavskiy/Anime/services/Impl/UserInfoService.java
+++ src/main/java/com/vladislavskiy/Anime/services/Impl/UserInfoService.java
@@ -1,7 +1,7 @@
 package com.vladislavskiy.Anime.services.Impl;
 
 
-import com.vladislavskiy.Anime.entity.UserInfo;
+import com.vladislavskiy.Anime.models.UserInfo;
 import com.vladislavskiy.Anime.repositories.UserInfoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +21,13 @@ public class UserInfoService implements UserDetailsService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Optional<UserInfo> userDetail = repository.findByName(username);
 
         // Converting userDetail to UserDetails
-        return userDetail.map(UserInfoDetails::new)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+        return userDetail.map(UserInfoDetails::new).orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
     }
 
     public String addUser(UserInfo userInfo) {
@@ -37,8 +35,8 @@ public class UserInfoService implements UserDetailsService {
         repository.save(userInfo);
         return "User Added Successfully";
     }
+
     public Boolean checkUserOnExistByUsername(String username) {
-        log.info("USERNAME "+username);
         Optional<UserInfo> userDetail = repository.findByName(username);
         return userDetail.isPresent();
     }
