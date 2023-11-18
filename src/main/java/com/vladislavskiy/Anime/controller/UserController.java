5e3c13b125d4c36c6331ecd4afc98c91b3322a83
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-19 01:38:51 +0200

    shorted code

--- src/main/java/com/vladislavskiy/Anime/controller/UserController.java
+++ src/main/java/com/vladislavskiy/Anime/controller/UserController.java
@@ -1,15 +1,9 @@
 package com.vladislavskiy.Anime.controller;
 
 import com.vladislavskiy.Anime.dto.UsernameRequest;
-import com.vladislavskiy.Anime.entity.AuthRequest;
-import com.vladislavskiy.Anime.entity.UserInfo;
-import com.vladislavskiy.Anime.services.Impl.JwtService;
+import com.vladislavskiy.Anime.models.UserInfo;
 import com.vladislavskiy.Anime.services.Impl.UserInfoService;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,49 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserInfoService service;
 
-    private final JwtService jwtService;
-
-    private final AuthenticationManager authenticationManager;
-
-    public UserController(UserInfoService service, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public UserController(@Qualifier("userInfoService") UserInfoService service) {
         this.service = service;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
     }
 
-    @GetMapping("/welcome")
-    public String welcome() {
-        return "Welcome this endpoint is not secure";
-    }
     @PostMapping("/isUserExistByName")
     public String isUserWithNameExist(@RequestBody UsernameRequest request) {
         return service.checkUserOnExistByUsername(request.username()).toString();
     }
+
     @PostMapping("/addNewUser")
     public String addNewUser(@RequestBody UserInfo userInfo) {
         return service.addUser(userInfo);
-    }
-
-    @GetMapping("/user/userProfile")
-    @PreAuthorize("hasAuthority('ROLE_USER')")
-    public String userProfile() {
-        return "Welcome to User Profile";
-    }
-
-    @GetMapping("/admin/adminProfile")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String adminProfile() {
-        return "Welcome to Admin Profile";
-    }
-
-    @PostMapping("/generateToken")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getUsername());
-        } else {
-            throw new UsernameNotFoundException("invalid user request !");
-        }
     }
 
 }
