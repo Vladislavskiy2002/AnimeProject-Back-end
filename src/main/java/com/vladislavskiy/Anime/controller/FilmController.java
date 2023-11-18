e54e915d55cc493ed65a59a43a73b0679b1389e9
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-19 01:34:48 +0200

    cleaned code

--- src/main/java/com/vladislavskiy/Anime/controller/FilmController.java
+++ src/main/java/com/vladislavskiy/Anime/controller/FilmController.java
@@ -2,15 +2,15 @@ package com.vladislavskiy.Anime.controller;
 
 
 import com.vladislavskiy.Anime.dto.FilmCredentialsDto;
-import com.vladislavskiy.Anime.repositories.FilmRepository;
+import com.vladislavskiy.Anime.handler.FilmNotFoundException;
+import com.vladislavskiy.Anime.models.Film;
+import com.vladislavskiy.Anime.services.FilmService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -18,74 +18,54 @@ import java.util.List;
 @CrossOrigin
 @Slf4j
 public class FilmController {
-    final private FilmRepository repository;
-    public FilmController(FilmRepository repository) {
-        this.repository = repository;
+    final private FilmService service;
+
+    public FilmController(FilmService service) {
+        this.service = service;
     }
+
     @GetMapping("/film")
-    public ResponseEntity getAllFilms() throws SQLException {
-        return repository.getAllFilms();
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
     }
+
     @GetMapping("/film/{id}")
-    public ResponseEntity getFilmById(@PathVariable Integer id) throws SQLException {
-        return repository.getFilmById(id);
+    public ResponseEntity<?> getFilmById(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<Film>(service.getFilmById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .header("Custom-Header", "значення")
+                    .body("message: Film with current id: " + id + " hasn't found");
+        }
     }
+
     @PostMapping("/film")
-    public ResponseEntity addNewFilm(@RequestBody FilmCredentialsDto filmCredentialsDto) throws SQLException {
-        log.info(filmCredentialsDto.toString());
-        return repository.addFilmByCredentials(filmCredentialsDto);
-    }
-    @GetMapping("/count")
-    public int getVideoCount() throws Exception {
-        // Отримати кількість вже існуючих відеофайлів
-        List<Path> existingVideos = getAllVideoPaths();
-        return existingVideos.size();
+    public ResponseEntity<Film> addNewFilm(@RequestBody FilmCredentialsDto filmCredentialsDto) {
+        return new ResponseEntity<>(service.addFilmByCredentials(filmCredentialsDto), HttpStatus.OK);
     }
 
     @PostMapping("/video")
     public String uploadVideo(@RequestParam("file") MultipartFile file) {
         try {
-            // Отримати шлях до каталогу для збереження відео
-            Path uploadPath = Paths.get("D:/frontend/anime-frontend-app/src/assets");
-
-            // Забезпечте існування каталогу
-            Files.createDirectories(uploadPath);
-
-            // Отримати кількість вже існуючих відеофайлів
-            List<Path> existingVideos = getAllVideoPaths();
-
-            // Генерувати нове ім'я файлу на основі кількості вже існуючих відео
-            String fileName = generateFileName(existingVideos.size() + 1);
-
-            // Збереження відеофайлу на файловій системі з новим іменем
-            Path filePath = uploadPath.resolve(fileName);
-            Files.copy(file.getInputStream(), filePath);
-
-            return fileName;
+            return service.uploadVideo(file);
         } catch (Exception e) {
             return "Error uploading video: " + e.getMessage();
         }
     }
 
-    // Метод для генерації імені файлу на основі кількості вже існуючих відеофайлів
-    private String generateFileName(int fileCount) {
-        return fileCount + ".mp4";
-    }
-
-    // Метод для отримання всіх відеофайлів у каталозі
-    private List<Path> getAllVideoPaths() throws Exception {
-        // Отримайте шлях до каталогу для збереження відео
-        Path uploadPath = Paths.get("D:/frontend/anime-frontend-app/src/assets");
-
-        // Отримати список всіх відеофайлів
-        return Files.list(uploadPath).filter(Files::isRegularFile).toList();
-    }
     @PutMapping("/film")
-    public ResponseEntity updateFilm(@RequestBody FilmCredentialsDto filmCredentialsDto) throws SQLException {
-        return repository.updateFilmByCredentials(filmCredentialsDto);
+    public ResponseEntity<Film> updateFilm(@RequestBody FilmCredentialsDto filmCredentialsDto) {
+        return new ResponseEntity<>(service.updateFilmByCredentials(filmCredentialsDto), HttpStatus.OK);
     }
+
     @DeleteMapping("/film/{id}")
-    public ResponseEntity deleteFilm(@PathVariable Integer id) throws SQLException {
-        return repository.deleteFilmById(id);
+    public ResponseEntity<?> deleteFilm(@PathVariable Long id) throws SQLException {
+        try {
+            service.deleteFilmById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (FilmNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
