7b92a03136bc2f2294a92bc5be53bb42c8b3a99e
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-11-19 01:40:57 +0200

    cleaned code

--- src/main/java/com/vladislavskiy/Anime/dto/UsernameRequest.java
+++ src/main/java/com/vladislavskiy/Anime/dto/UsernameRequest.java
@@ -1,9 +1,3 @@
 package com.vladislavskiy.Anime.dto;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 public record UsernameRequest(String username) { }
